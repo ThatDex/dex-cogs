cb9c0177e28e66f399bcb8e1f42f88fe30cc4dbf
Dex <Brodie@dexnet.ca>
2016-04-25 22:54:34 -0300

    Fixed indents
    Indentation was wrong. Fixed now.

--- GoogleCalendar/GCalender.py
+++ GoogleCalendar/GCalender.py
@@ -23,32 +23,32 @@ class GCalender:
         await self.bot.say("I can do stuff!")
 
 	def get_creds():
-    """Gets valid user credentials from storage.
+	    """Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+	    If nothing has been stored, or if the stored credentials are invalid,
+	    the OAuth2 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+	    Returns:
+	        Credentials, the obtained credential.
+	    """
+	    home_sdir = os.path.expanduser('~')
+	    credential_dir = os.path.join(home_dir, '.credentials')
+	    if not os.path.exists(credential_dir):
+	        os.makedirs(credential_dir)
+	    credential_path = os.path.join(credential_dir,
+	                                   'calendar-python-quickstart.json')
 
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+	    store = oauth2client.file.Storage(credential_path)
+	    credentials = store.get()
+	    if not credentials or credentials.invalid:
+	        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+	        flow.user_agent = APPLICATION_NAME
+	        if flags:
+	            credentials = tools.run_flow(flow, store, flags)
+	        else: # Needed only for compatibility with Python 2.6
+	            credentials = tools.run(flow, store)
+	        print('Storing credentials to ' + credential_path)
+	    return credentials
 
 def setup(bot):
 	get_creds()
