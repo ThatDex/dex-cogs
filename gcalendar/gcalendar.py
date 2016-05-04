3c767126bc0498e33db55a2d259da9e892436305
Brodie "Dex" Larabee <Brodie@dexnet.ca>
2016-05-03 21:36:30 -0300

    Tabbing the tab

--- gcalendar/gcalendar.py
+++ gcalendar/gcalendar.py
@@ -53,11 +53,11 @@ class gcalender:
 			eventList.append(start + " " + ev_summary)
 
 	@commands.group(no_pm=True, pass_context=True)
-    @checks.mod_or_permissions(manage_messages=True)
-    async def scheduler(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await self.bot.say("Error")
-            return
+	@checks.mod_or_permissions(manage_messages=True)
+	async def scheduler(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await self.bot.say("Error")
+			return
 
 	@gcalendar.command()
 	async def _gcalendar_tenapps(self):
