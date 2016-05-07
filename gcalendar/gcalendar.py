9a4f53d371e3859aeb8d896cc68557bff6b16f54
Brodie "Dex" Larabee <Brodie@dexnet.ca>
2016-05-07 19:02:10 -0300

    Forgetmenot

--- gcalendar/gcalendar.py
+++ gcalendar/gcalendar.py
@@ -276,11 +276,11 @@ class gcalender:
 						if 'T' in start:
 							startformat = start.replace('T', ' │ ').replace('+', ' │ +')
 							ev_summary = event['summary']
-							eventList.append("│ " +startformat + "  │ " + ev_format)
+							eventList.append("│ " +startformat + "  │ " + ev_summary)
 							
 						if 'T' not in start:
 							ev_summary = event['summary']
-							eventList.append("│ " +start + " │" + " ALL-DAY  │ ALL-DAY │ " + ev_format)
+							eventList.append("│ " +start + " │" + " ALL-DAY  │ ALL-DAY │ " + ev_summary)
 
 					if ((len(str(eventList))) - len(eventList)) < 1950:
 						await self.bot.say("```" + "\n" + "| Date       | Time     | UTC     | Event" + "\n" 
